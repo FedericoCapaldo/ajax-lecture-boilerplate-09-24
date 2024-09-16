88fa9506481ac8297c3dda240f309e35edabf8ee
Federico Capaldo <federico.capaldo@hotmail.com>
2024-09-16 23:04:32 +0100

    making actual request to an API with an AJAX request

--- javascript/application.js
+++ javascript/application.js
@@ -5,9 +5,20 @@ const signUp = (event) => {
 
   const email = document.getElementById("email").value
   const password = document.getElementById("password").value
+  const requestBody = JSON.stringify({ "email": email, "password": password });
 
-  console.log(email)
-  console.log(password)
+  const url = "https://reqres.in/api/register"
+  const requestData = {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: requestBody
+  }
+
+  fetch(url, requestData)
+    .then((response) => response.json())
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err))
+
 }
 
 
