94d614d1159ca2e968c0cff49000059fa01d87fc
Federico Capaldo <federico.capaldo@hotmail.com>
2024-09-16 22:17:31 +0100

    retrivieng email and password to send in request

--- javascript/application.js
+++ javascript/application.js
@@ -2,7 +2,12 @@ const form = document.getElementById("form")
 
 const signUp = (event) => {
   event.preventDefault() // prevent form from being submitted automatically to the url expressed in action on the form
-  console.log('submitted!')
+
+  const email = document.getElementById("email").value
+  const password = document.getElementById("password").value
+
+  console.log(email)
+  console.log(password)
 }
 
 
